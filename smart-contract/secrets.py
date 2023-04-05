d9a6f9001967ce9eb61ed03916e2655f7de504a1
Jason Schreiber <jasonschreiber@jasons-mbp.myfiosgateway.com>
2023-04-05 01:10:02 -0400

    updating secrets.py file to include mnemonics and token

--- smart-contract/secrets.py
+++ smart-contract/secrets.py
@@ -2,10 +2,12 @@
 account_mnemonics = [
     # "sample mnemonic 1",
     # "sample mnemonic 2"
+"anger patrol isolate dust gesture laugh sweet uniform elevator nuclear crawl pluck puppy crop bus little never disagree today grocery stay volcano correct abandon churn",
+"impact velvet radio flower river giraffe timber install size opera business drink wet drama penalty flock edge nasty ticket rhythm busy pilot average ability abstract"
 ]
 
 # PureStake API token
-algod_headers = {"X-API-Key": "Purestake API Key"}
+algod_headers = {"X-API-Key": "kP24aojJjz5ulq9hW4Mix6lhigQLQJkf3ANfiIGF"}
 
 # PureStake algod address
 algod_address = "https://testnet-algorand.api.purestake.io/ps2"
