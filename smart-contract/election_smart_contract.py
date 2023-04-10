f91037983f33de139aa75540b19b196be662aa72
Jason Schreiber <jasonschreiber@hnt-04898.apn.wlan.private.upenn.edu>
2023-04-10 16:54:13 -0400

    fixed on_closeout and clear_state_program syntax

--- smart-contract/election_smart_contract.py
+++ smart-contract/election_smart_contract.py
@@ -33,6 +33,7 @@ def approval_program():
 
     # value of whether or not the sender can vote ("yes", "no", or "maybe")
     get_sender_can_vote = App.localGetEx(Int(0), App.id(), Bytes("can_vote"))
+    get_can_vote = App.localGetEx(Txn.application_args[1], App.id(), Bytes("Maybe"))
 
     # get_vote_sender is a value that the sender voted for,
     #   a number indicating the index in the VoteOptions string faux-array.
@@ -49,13 +50,15 @@ def approval_program():
             # Removes the user's vote from the correct vote tally if and only if the user closes out of program
             # before the end of the election. Otherwise, does nothing
             If(
-                And(
-                    get_vote_of_sender.hasValue(),
-                ),
-                App.globalPut(
-                    get_vote_of_sender.value(),
-                    App.globalGet(get_vote_of_sender.value()) - Int(1),
-                ),
+                get_vote_of_sender.hasValue(),
+            ).Then(
+                Seq([
+                    App.globalPut(Concat(Bytes('VotesFor'),
+                                         itoa(get_vote_of_sender.value())),
+                    App.globalGet(Concat(Bytes('VotesFor')),
+                                  itoa(get_vote_of_sender.value()))) - Int(1),
+                    App.localDel(Int(0), Bytes('voted')),
+                ])
             ),
             Return(Int(1)),
         ]
@@ -72,14 +75,37 @@ def approval_program():
 
     on_update_user_status = Seq(
         # TODO: UPDATE USER LOGIC
+        get_can_vote,
+        # assert only the creator can approve/disapprove
+        Assert(is_creator),
+        # AND can only be be approved before election ends
+        Assert(Global.round() < App.globalGet(Bytes('ElectionEnd'))),
+        # fetch the creator's decision to approve/reject user account
+        # update the user's voting status accordingly by setting user's can_vote local state
+        # AND creator cannot update more than once
         Return(Int(1))
     )
 
     choice = Btoi(Txn.application_args[1])
     on_vote = Seq(
         # TODO: USER VOTING LOGIC:
-        [
 
+        [
+            # assert that the election is not over and user is allowed to vote
+            # if user already voted
+            get_vote_of_sender,
+            Assert(Global.round() < App.globalGet(Bytes('ElectionEnd'))),
+            # Assert(user allowed to vote),
+            If(
+                And(
+                    get_vote_of_sender.hasValue(),
+                ),
+            ),
+            # return a 0
+            Return(Int(0)),
+            # Assert the vote choice (it's above before the seq) is within index bounds of vote options
+            # update vote tally for user's choice under corresponding global vars
+            # record the user's vote index in acct local storage under key 'voted'
             Return(Int(1))
         ]
     )
@@ -94,8 +120,8 @@ def approval_program():
         [Txn.on_completion() == OnComplete.CloseOut, on_closeout],
         [Txn.on_completion() == OnComplete.OptIn, on_register],
         # 1.1: the cases that will trigger the update_user_status and on_vote sequences
-        [Txn.on_completion() == OnComplete.Vote, on_vote],
-        [Txn.on_completion() == OnComplete.UpdateUserStatus, on_update_user_status]
+        [Txn.application_args[0] == Bytes["vote"], on_vote],
+        [Txn.application_args[0] == Bytes["update_user_status"], on_update_user_status]
     )
 
     return program
@@ -105,14 +131,32 @@ def clear_state_program():
     """ Handles the logic of when an account clears its participation in a smart contract. """
 
     # TODO: CLEAR STATE PROGRAM
-
+    # Just like the close_out sequence,
+    # if the user clears state of program before the end of voting period
+    # then it removes their vote from the correct vote tally.
+    # Otherwise, it doesn't do anything.
     get_vote_of_sender = App.localGetEx(Int(0), App.id(), Bytes("voted"))
 
     program = Seq(
         # remove their vote from the correct vote tally
         [
-
-            Return(Int(1))
+            get_vote_of_sender,
+            # called when user removes interaction with this smart contract from their account.
+            Assert(Global.round() < App.globalGet(Bytes('ElectionEnd'))),
+            # Removes the user's vote from the correct vote tally if and only if the user closes out of program
+            # before the end of the election. Otherwise, does nothing
+            If(
+                get_vote_of_sender.hasValue(),
+            ).Then(
+                Seq([
+                    App.globalPut(Concat(Bytes('VotesFor'),
+                                         itoa(get_vote_of_sender.value())),
+                                  App.globalGet(Concat(Bytes('VotesFor')),
+                                                itoa(get_vote_of_sender.value()))) - Int(1),
+                    App.localDel(Int(0), Bytes('voted')),
+                ])
+            ),
+            Return(Int(1)),
         ]
     )
 
